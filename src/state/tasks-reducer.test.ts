8765202fefb9087048604f1b9b688f5cf6417b63
antonymss <demeshkevichanton@gmail.com>
2021-02-11 12:57:20 +0300

    refact

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -55,18 +55,6 @@ test('correct task should be added to correct array', () => {
 })
 
 test('status of specified task should be changed', () => {
-    const startState: TaskStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
 
     const action = changeTaskStatusAC("2", false, "todolistId2");
 
