2dce58ee403bd007199dac7059e19639a5820570
antonymss <demeshkevichanton@gmail.com>
2021-02-11 12:57:04 +0300

    refact

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -42,18 +42,6 @@ test('correct task should be deleted from correct array', () => {
 });
 
 test('correct task should be added to correct array', () => {
-    const startState: TaskStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
 
     const action = addTaskAC("juce", "todolistId2");
 
