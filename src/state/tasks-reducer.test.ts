80300a8254f9aed32deb216e8272286f1e9bc461
antonymss <demeshkevichanton@gmail.com>
2021-02-11 12:57:54 +0300

    refact

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -75,18 +75,6 @@ test('title of specified task should be changed', () => {
 });
 
 test('new array should be added when new todolist is added', () => {
-    const startState: TaskStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
 
     const action = AddTodolistAC("new todolist");
 
