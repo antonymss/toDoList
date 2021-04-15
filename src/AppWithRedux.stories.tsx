33518dc3e4b885465257d2ec798e76093fb33d5f
antonymss <demeshkevichanton@gmail.com>
2021-04-15 14:00:42 +0300

    refact

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import {Meta, Story} from '@storybook/react/types-6-0';
-import AppWithRedux from "./AppWithRedux";
-import {ReduxStoreProviderDecorator} from "./stories/decorators/ReduxStoreProviderDecorator";
 
 
-export default {
-    title: 'Todolist/AppWithRedux',
-    component: AppWithRedux,
-    decorators: [ReduxStoreProviderDecorator]
-} as Meta;
-
-const Template: Story= () => <AppWithRedux />;
-
-export const AppWithReduxExample = Template.bind({});
+//
+// export default {
+//     title: 'Todolist/AppWithRedux',
+//     component: AppWithRedux,
+//     decorators: [ReduxStoreProviderDecorator]
+// } as Meta;
+//
+// const Template: Story= () => <AppWithRedux />;
+//
+// export const AppWithReduxExample = Template.bind({});
 
 
