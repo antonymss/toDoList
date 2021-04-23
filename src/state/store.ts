9142883a3fbf40a5f574cab287900d322d77a8b9
antonymss <demeshkevichanton@gmail.com>
2021-04-23 12:18:57 +0300

    refact

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,11 +2,13 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
 import thunkMiddleware from "redux-thunk";
+import {appReducer} from "../app-reducer";
 
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todolistsReducer
+    todolists: todolistsReducer,
+    app: appReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
