aeaf10d67e03962f3afb1ec6413bf06942ac437a
antonymss <demeshkevichanton@gmail.com>
2021-04-25 10:17:47 +0300

    refact

--- src/components/EditableSpan/EditableSpan.stories.tsx
+++ src/components/EditableSpan/EditableSpan.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import AddItemForm, {AddItemFormType} from "./AddItemForm";
+import AddItemForm, {AddItemFormType} from "../AddItemForm/AddItemForm";
 import {action} from "@storybook/addon-actions";
 import {EditableSpan, EditableSpanPropsType} from "./EditableSpan";
 
