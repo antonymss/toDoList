5a1bd4bc71105bdd5632ce7906ee611fcec92e4b
antonymss <demeshkevichanton@gmail.com>
2021-04-25 10:17:29 +0300

    refact

--- src/app-reducer.ts
+++ src/app-reducer.ts
@@ -1,7 +1,8 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType
+    status: 'loading' as RequestStatusType,
+    error: null as null | string
 }
 
 type InitialStateType = typeof initialState
@@ -10,9 +11,21 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
-type ActionsType = any
+export const setAppStatusAC = (status: RequestStatusType) => {
+    return ({type: 'APP/SET-STATUS', status} as const)
+}
+export const setAppErrorAC = (error: string | null) => {
+    return ({type: 'APP/SET-ERROR', error} as const)
+}
+
+export type setAppStatusType = ReturnType<typeof setAppStatusAC>
+export type setAppErrorType = ReturnType<typeof setAppErrorAC>
+
+type ActionsType = setAppStatusType | setAppErrorType
