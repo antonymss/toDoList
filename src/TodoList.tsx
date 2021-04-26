8f5b2d900909695f059276149b37298a0240a251
antonymss <demeshkevichanton@gmail.com>
2021-04-26 10:56:04 +0300

    refact

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect} from "react";
 import {FilterValuesType} from "./App";
-import AddItemForm from "./AddItemForm";
-import {EditableSpan} from "./EditableSpan";
+import AddItemForm from "./components/AddItemForm/AddItemForm";
+import {EditableSpan} from "./components/EditableSpan/EditableSpan";
 import {Delete} from "@material-ui/icons";
 import {Button, IconButton} from "@material-ui/core";
 import {Task} from "./Task";
